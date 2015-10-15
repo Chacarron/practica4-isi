0cbb62d83cd8d43a54a290d6a6917f3f04e97920
Jona <jona@jona-VirtualBox.(none)>
2015-10-15 18:17:06 +0200

    test de prueba como plantilla

---
+++ todos/tests/jasmine/client/unit/ClientSpec.js
@@ -0,0 +1,33 @@
+describe('data', function () {
+    it('Debe devolver la lista de paginas cuando un usuario se logea', function () {
+      var currentList = 0;
+      var currentUser = 0;
+      var result = {_id: currentList,createdBy: currentUser};
+      spyOn(Lists, 'findOne').and.returnValue(result);
+
+      //expect(Router.route.data()).toBe(result);
+      expect(Lists.findOne.calls.argsFor(0)).toEqual([]);
+    });
+  });
+describe('lists', function () {
+    it('Debe ........' , function () {
+      var currentUser = 0;
+      var result = {createdBy:  currentUser};
+      spyOn(Lists, 'find').and.returnValue(result);
+
+      //expect(lists()).toBe(result);
+      expect(Lists.find.calls.argsFor(0)).toEqual([]);
+    });
+  });
+
+describe('createNewList', function () {
+    it('Debe crear una nueva lista' , function () {
+      var currentUser = 1;
+      var listName = "name";
+      var result = {name: listName,ceatedBy: currentUser};
+      spyOn(Lists, 'insert').and.returnValue(result);
+
+      //expect(Meteor.call('createNewList')).toBe(result);
+      expect(Lists.insert.calls.argsFor(0)).toEqual([]);
+    });
+  });
