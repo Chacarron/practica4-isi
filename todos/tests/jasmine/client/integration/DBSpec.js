bf55810595a9fa41d53c3e7065278ebd5998f612
Alejandro <alejanm09@gmail.com>
2015-10-20 18:24:13 +0200

    Actualizacion

--- todos/tests/jasmine/client/integration/DBSpec.js
+++ todos/tests/jasmine/client/integration/DBSpec.js
@@ -12,11 +12,36 @@ describe("Database Test", function () {
 
 });
 
-describe("Database Test", function () {
-
-    $("#adios").val("GOD")
-    $("#adios").submit()
+describe("Database Test2", function () {
     
-//comprobar que ha creado el il
+  it("Añade un elemento a la lista", function () {
+    var nombre = "Comer";
+    var userId = Meteor.userId();
+
+    $("#adios").val(nombre);
+    $("#adios").submit();
+
+    setTimeout(function () {
+      expect(Lists.findOne({name: nombre, 
+                            createdBy: userId})).toBe(true);
+      done();
+    },100);
+  });
+
+  it("Se registra un usuario", function () {
+    var nombre = "god@gmail.com";
+    var contrasena = "godmode";
+    var userId = Meteor.userId();
+
+    $("#emailRegister").val(nombre);
+    $("#passwordRegister").val(contrasena);
+    $("#reg").submit();
+
+    setTimeout(function () {
+      expect(Todos.findOne({name: nombre, 
+                            createdBy: userId})).toBe(true);
+      done();
+    },100);
+  });
 
 });
