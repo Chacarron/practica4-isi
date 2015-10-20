3ebc104d54c2c029fae6ff097a8ae85824487874
Alejandro <alejanm09@gmail.com>
2015-10-20 20:31:28 +0200

    Subida prueba

--- todos/tests/jasmine/client/integration/DBSpec.js
+++ todos/tests/jasmine/client/integration/DBSpec.js
@@ -50,4 +50,13 @@ describe("Database Test2", function () {
     },100);
   });
 
+  it("Meter un Task", function () {
+    var tarea = "cocinar";
+    var userId = Meteor.userId();
+
+    $("#putName").val(tarea);
+    $("#putName").submit();
+    expect($("[name=todoName]")).toBe(tarea);
+  });
+
 });
